f21fa9475d960d03606480f2a7c13137e92d8d94
vedikaS-byte <vedika.shirtekar@gmail.com>
2026-04-15 20:44:45 +0000

    Experiment with sql script in R.

--- lab_scripts/week3.sql
+++ lab_scripts/week3.sql
@@ -1,2 +1,6 @@
-CREATE TABLE species_2 AS
-  SELECT * FROM read_csv("data/species (1).csv");
+--CREATE TABLE species_2 AS
+--  SELECT * FROM read_csv("data/species (1).csv");
+  
+  
+CREATE TABLE power AS
+  SELECT * FROM read_csv_auto('/data/DATA/power/electricity-generation_emissions_sources_v5_5_0.csv');
