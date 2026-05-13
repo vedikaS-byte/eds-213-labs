1230715d25c7a9cff552b13defc2d072b99788a4
Vedika Shirtekar <vedikashirtekar@169-231-175-77.wireless.ucsb.edu>
2026-05-13 00:09:49 -0700

    General update (Repo structure in progress).

--- build-my-database.sql
+++ build-my-database.sql
@@ -1,5 +1,3 @@
----  Main question: How do emissions from residential and non-residential buildings change across seasons, 
---                  and how do these patterns align with power plant emissions? 
 
 -- Import data
 
@@ -42,11 +40,14 @@ CSV HEADER;
 
 -- Question: Which US counties emit the most CO₂ from non-residential fuel use per square meter of building floor area?
 -- Note: Database contains two tables (sources with PK source_id, and emission_records with FK source_id)
-SELECT s.source_name, s.iso3_country, SUM(e.emissions_quantity), s.capacity,
-    ROUND(SUM(e.emissions_quantity) / (s.capacity), 6) AS co2_per_m2
-    FROM sources s
-    JOIN emission_records e ON s.source_id = e.source_id
-    WHERE e.sector LIKE 'buildings'
-    GROUP BY s.source_name, s.iso3_country, s.capacity
-    ORDER BY co2_per_m2 DESC;
+SELECT s.source_name, 
+    ROUND(
+        SUM(e.emissions_quantity) / s.capacity 
+           / COUNT(DISTINCT EXTRACT(YEAR FROM e.start_time)), 3) AS    
+           Co2_per_m2_per_yeaR 
+         FROM sources s
+         JOIN emission_records e ON s.source_id = e.source_id
+         WHERE e.sector = 'buildings' AND s.capacity_units = 'm^2'
+         GROUP BY s.source_name, s.capacity
+          ORDER BY co2_per_m2_per_year DESC;
 
